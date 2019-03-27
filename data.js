09aa47ee5514a08676eed015f6d7edd179c21fd4
Uli Orduno <uli.orduno@codeimmersives.com>
2019-03-27 15:25:49 -0400

    adds Data

--- data.js
+++ data.js
@@ -1,29 +1,39 @@
 const data = [{
     name: 'Leticia Mel',
-    occupation: 'Detective'
+    occupation: 'Detective',
     isMainCharacter: true,
     age: 30,
-    gender: 'Transgender Woman'
-    bio: 
-    }
+    gender: 'Transgender Woman',
+    bio: ['Private Detective for hire', 'Meets Mr. Malcolm on the subway', 
+    'neice of famamous inspector Schlomo Piere',
+    'inherited his vast estate after his death', 
+    'tranitioned her gender from male to female',
+    'is an expert hacker', 'doesnt take no for an answer']
+
+    },
     {
 
     name: 'Mr. Malcolm',
-    occupation: 'Medical Patient Transporter'
+    occupation: 'Medical Patient Transporter',
     isMainCharacter: true,
     age: 38,
-    gender: 'Male'
-    bio:
+    gender: 'Male',
+    bio:['Woke up in Central Park near a womam who had neen savagely disembowelled', 
+    'runs before the police can arrive', 'manages to evade police as he enters the subway',
+    'runs into a kind lady who offers her help', 'turns himself into police when he is when he is identified as suspect',
+    'jailed'
+    ]
 
-    }
+    },
     {   
     name: 'Gizbon ',
     occupation: 'Hairless Dog',
     isMainCharacter: false,
     age: 2,
     gender: 'neutered',
-    bio:
-    }
+    bio:[ 'is a chinese crested dog', 'was recently neutered', 
+    'misses his owner']
+    },
     {
 
     name: 'Kyle Klamp',
@@ -31,34 +41,49 @@ const data = [{
     isMainCharacter: true,
     age: 55,
     gender: 'male',
-    bio:
+    bio:['closest confidant to Schlomo Pierre', 'works for the NYPD homicide departmnet',
+    'was a common fixture in Mels childhood and adoloscence', 'does as he is told']
 
 
-    }
+    },
 {
     name: 'Judge Beyer',
     occupation: 'judge',
     isMainCharacter: false,
     age: 67,
-    gender: 'male'
-    bio:
+    gender: 'male',
+    bio:['mostly always rules against defendants of the working class', 'sentences them to mining', 'is corrupt',
+    'is presiding over Mr. Malcolms case']
 
-}
+},
 {
     name: 'Wanda Castor',
     occupation: 'hacker for hire and criminal in general',
     isMainCharacter: false,
     age: 40,
     gender: 'female',
-    bio:    
-}
+    bio:  ['is known by several names', 'has been a witness in Judge Beyers court on more than one occasion',
+    'is a hacker for hire']
+
+    
+},
+{
     name: 'Wylie',
     occupation: 'Animal Hospital Attendant',
-    isMainCharacter: false
+    isMainCharacter: false,
     age: 26,
     gender: 'male',
-    bio:
+    bio: ['known drug dealer of illicit drugs', 'employed at the animal hospital',
+    'last person seen with Mr. Malcolm on the day prior to the murder', 'shifty', 'sketchy' ]
 
+},
+{
+    name:'Cassandra Clarissa',
+    occupation: 'lawyer',
+    isMainCharacter: false,
+    age:27,
+    gender: 'female',
+    bio:['hired to represent Mr.Malcom', 'is an inexperienced lawyer', 'hired by Mel to follow her defense strategy']
 }
     
 ]
