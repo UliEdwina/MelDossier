3eab64cc1de8e5220517374c0cefd47e8b0baafd
Uli Orduno <uli.orduno@codeimmersives.com>
2019-03-27 14:45:50 -0400

    initial commit

--- data.js
+++ data.js
@@ -1,3 +1,64 @@
-const data = [
+const data = [{
+    name: 'Leticia Mel',
+    occupation: 'Detective'
+    isMainCharacter: true,
+    age: 30,
+    gender: 'Transgender Woman'
+    bio: 
+    }
+    {
+
+    name: 'Mr. Malcolm',
+    occupation: 'Medical Patient Transporter'
+    isMainCharacter: true,
+    age: 38,
+    gender: 'Male'
+    bio:
+
+    }
+    {   
+    name: 'Gizbon ',
+    occupation: 'Hairless Dog',
+    isMainCharacter: false,
+    age: 2,
+    gender: 'neutered',
+    bio:
+    }
+    {
+
+    name: 'Kyle Klamp',
+    occupation:'NYPD Detective',
+    isMainCharacter: true,
+    age: 55,
+    gender: 'male',
+    bio:
+
+
+    }
+{
+    name: 'Judge Beyer',
+    occupation: 'judge',
+    isMainCharacter: false,
+    age: 67,
+    gender: 'male'
+    bio:
+
+}
+{
+    name: 'Wanda Castor',
+    occupation: 'hacker for hire and criminal in general',
+    isMainCharacter: false,
+    age: 40,
+    gender: 'female',
+    bio:    
+}
+    name: 'Wylie',
+    occupation: 'Animal Hospital Attendant',
+    isMainCharacter: false
+    age: 26,
+    gender: 'male',
+    bio:
+
+}
     
 ]
